2431892a19f84055f96b3fdb54b984f68063c0b4
JoshuaParkSJ <jpark9061@gmail.com>
2020-11-12 20:21:53 -0800

    feat: get and add item service

--- webdev-workshop-ui/src/components/services/cartService.js
+++ webdev-workshop-ui/src/components/services/cartService.js
@@ -6,6 +6,16 @@ export const getShoppingCart = async request => {
 };
 
 export const addShoppingCart = async request => {
-  const response = await axios.patch('http://localhost:3003/cart/add', request);
+  const response = await axios.post('http://localhost:3003/cart/add', request);
+  return response.data;
+}
+
+export const getItem = async request => {
+  const response = await axios.get('http://localhost:3003/item/get', request);
+  return response.data;
+}
+
+export const addItem = async request => {
+  const response = await axios.patch('http://localhost:3003/item/add', request);
   return response.data;
 }
