0fde48eb4c30df9a1a4a636402c868133ad4f426
JoshuaParkSJ <jpark9061@gmail.com>
2020-11-12 18:35:23 -0800

    feat: carousel

--- webdev-workshop-ui/src/components/carousel/Carousel.js
+++ webdev-workshop-ui/src/components/carousel/Carousel.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Row } from 'react-flexbox-grid';
+import { StyledCol, StyledImage } from './StyledCarousel';
+import Carousel1 from '../../images/carousel1.jpg';
+import Carousel2 from '../../images/carousel2.jpg';
+import Carousel3 from '../../images/carousel3.jpg';
 
 const Carousel = () => {
-  return null;
+  return (
+    <Row>
+      <StyledCol xs>
+        <StyledImage src={Carousel1} alt="" />
+      </StyledCol>
+      <StyledCol xs>
+        <StyledImage src={Carousel2} alt="" />
+      </StyledCol>
+      <StyledCol xs>
+        <StyledImage src={Carousel3} alt="" />
+      </StyledCol>
+    </Row>
+  );
 }
 
 export default Carousel;
---
+++ webdev-workshop-ui/src/components/carousel/StyledCarousel.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { Col } from 'react-flexbox-grid';
+
+export const StyledCol = styled(Col)`
+  padding: 0px 0px;
+`
+
+export const StyledImage = styled.img`
+  width: 100%;
+  height: 90%;
+`
