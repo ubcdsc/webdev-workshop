cba80810fa5987c08a5069aa947f2ec68bf75d3f
JoshuaParkSJ <jpark9061@gmail.com>
2020-11-12 21:22:51 -0800

    feat: dashboard

--- webdev-workshop-ui/src/components/dashboard/Dashboard.js
+++ webdev-workshop-ui/src/components/dashboard/Dashboard.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import { StyledInput } from './StyledDashboard';
+import { Box, Title, Subtitle, StyledInput, Text } from './StyledDashboard';
+import AppContext from '../context/AppContext';
 
 const Dashboard = () => {
+  const { cart } = useContext(AppContext);
+  const cartItems = () => {
+    cart?.map(item => {
+      return (
+        <Row>
+          <Text>{item.name}, {item.quantity}</Text>          
+        </Row>
+      )
+    })
+  }
   return (
     <Grid>
-      <Row>
-        <StyledInput />
-      </Row>
+      <Box>
+        <Row>
+          <Title>Dashboard</Title>
+        </Row>
+        <Row>
+          <Col>
+            <Row>
+              <Subtitle>Add Item</Subtitle>
+            </Row>
+            <Row>
+              <StyledInput placeholder="id" />
+            </Row>
+            <Row>
+              <StyledInput placeholder="name" />
+            </Row>
+            <Row>
+              <StyledInput placeholder="price" />
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <Subtitle>Cart</Subtitle>
+            </Row>
+            {cart ? cartItems(): null}
+          </Col>
+        </Row>
+      </Box>
     </Grid>
   )
 }
