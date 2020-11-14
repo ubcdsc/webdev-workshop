73a0c4412a5f685e3cf578f59159016ddedfe629
mertbarutcuoglu <kbarutcu@students.cs.ubc.ca>
2020-11-13 17:08:58 -0800

    feat: add product

--- webdev-workshop-ui/src/components/dashboard/Dashboard.js
+++ webdev-workshop-ui/src/components/dashboard/Dashboard.js
@@ -1,10 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useRef } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import { Box, Title, Subtitle, StyledInput, Text, ItemImage, StyledRow, StyledButton } from './StyledDashboard';
 import AppContext from '../context/AppContext';
 
 const Dashboard = () => {
   const { cart } = useContext(AppContext);
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [file, setFile] = useState('');
+  const fileRef = useRef();
+  
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const fileUploaded = event.target;
+    console.log(fileUploaded);
+    const file = fileUploaded.files;
+    console.log(file);
+  }
+
   const cartItems = () => {
     return cart.map(item => {
       return (
@@ -21,34 +36,37 @@ const Dashboard = () => {
         <StyledRow>
           <Title>Dashboard</Title>
         </StyledRow>
-        <Row>
-          <Col>
-            <Row>
-              <Subtitle>Add Item</Subtitle>
-            </Row>
-            <Row>
-              <StyledInput placeholder="id" />
-            </Row>
-            <Row>
-              <StyledInput placeholder="name" />
-            </Row>
-            <Row>
-              <StyledInput placeholder="price" />
-            </Row>
-            <Row>
-              <StyledInput type="file" />
-            </Row>
-            <Row>
-              <StyledButton type="submit" />
-            </Row>
-          </Col>
-          <Col>
-            <Row>
-              <Subtitle>Cart</Subtitle>
-            </Row>
-            {cart[0] ? cartItems(): null}
-          </Col>
-        </Row>
+          <Row>
+          <form onSubmit={handleSubmit}>
+            <Col>
+              <Row>
+                <Subtitle>Add Item</Subtitle>
+              </Row>
+              <Row>
+                <StyledInput placeholder="id" value={id} onChange={(e) => setId(e.target.value)}/>
+              </Row>
+              <Row>
+                <StyledInput placeholder="name" value={name}  onChange={(e) => setName(e.target.value)}/>
+              </Row>
+              <Row>
+                <StyledInput placeholder="price" value={price}  onChange={(e) => setPrice(e.target.value)}/>
+              </Row>
+              <Row>
+                <StyledInput type="file" ref={fileRef}  onChange={(e) => setFile()}/>
+              </Row>
+              <Row>
+                <StyledButton type="submit" />
+              </Row>
+            </Col>
+            </form>
+            <Col>
+              <Row>
+                <Subtitle>Cart</Subtitle>
+              </Row>
+              {cart[0] ? cartItems(): null}
+            </Col>
+          </Row>
+        
       </Box>
     </Grid>
   )
